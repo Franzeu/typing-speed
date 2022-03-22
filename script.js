4ba13c9ed7c7d340d6a2bdbbc31f880fa8457afb
Franz <43397903+Franzeu@users.noreply.github.com>
2022-03-22 04:32:42 -0700

    Created index.html and styles.css

---
+++ script.js
@@ -0,0 +1,5 @@
+const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random'
+const textDisplayElement = document.getElementById('textDisplay')
+const textInputElement = document.getElementById('textUserInput')
+const timerElement = document.getElementById('timer')
+
