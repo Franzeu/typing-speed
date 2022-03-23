68878a1e3c1d2a56e438474317c22b8c2c6063d1
Franz <43397903+Franzeu@users.noreply.github.com>
2022-03-23 02:49:29 -0700

    Fetches words.json file

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ const textDisplayElement = document.getElementById('textDisplay')
 const textInputElement = document.getElementById('textUserInput')
 const timerElement = document.getElementById('timer')
 
+// Fetches the json
+fetch("./words.json")
+.then(response => {
+   return response.json();
+})
+.then(data => console.log(data));
