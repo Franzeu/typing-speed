1e947e99b3eb37125e59e88001815b757ad64048
Franz <43397903+Franzeu@users.noreply.github.com>
2022-03-28 23:01:42 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random'
 const textDisplayElement = document.getElementById('textDisplay')
 const textInputElement = document.getElementById('textUserInput')
 const timerElement = document.getElementById('timer')
 
+
 // Fetches the json
-fetch("./words.json")
+fetch("./text/words.json")
 .then(response => {
    return response.json();
 })
 // Prints out the data that was fetched from the json file
-.then(data => console.log(data));
+.then(data => console.log(data.english));
 
