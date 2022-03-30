51830f005a959db8d812db20e546246fb2aeb5bb
Franz <43397903+Franzeu@users.noreply.github.com>
2022-03-29 22:11:03 -0700

    Added a countdown timer and displayed json array onto textDisplayContainer

--- script.js
+++ script.js
@@ -4,10 +4,32 @@ const timerElement = document.getElementById('timer')
 
 
 // Fetches the json
-fetch("./text/words.json")
-.then(response => {
-   return response.json();
-})
-// Prints out the data that was fetched from the json file
-.then(data => console.log(data.english));
+function getRandomWords() {
+   return fetch("./text/words.json")
+      .then(response => response.json())
+      .then(data => data.english)
+}
 
+async function renderNewQuote() {
+   const words = await getRandomWords()
+   textDisplayElement.innerHTML = words
+   
+   startTimer()
+}
+
+var startTime = 60;
+function startTimer() {
+   var startTimer = setInterval(function() {
+      if(startTime <= 0){
+         console.log("Time is up!");
+      }
+   
+      timerElement.innerText = startTime;
+      startTime -= 1;
+   
+   },1000)
+}
+
+
+
+renderNewQuote()
